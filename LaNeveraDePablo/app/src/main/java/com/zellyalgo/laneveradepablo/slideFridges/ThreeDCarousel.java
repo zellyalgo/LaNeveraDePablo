949e77a595d3757089c866df385b7e2ac461886c
ZellyAlgo <zell.y.algo@gmail.com>
2015-11-14 21:00:18 +0100

    REFACTOR variables changed

--- LaNeveraDePablo/app/src/main/java/com/zellyalgo/laneveradepablo/slideFridges/ThreeDCarousel.java
+++ LaNeveraDePablo/app/src/main/java/com/zellyalgo/laneveradepablo/slideFridges/ThreeDCarousel.java
@@ -1,7 +1,6 @@
 package com.zellyalgo.laneveradepablo.slideFridges;
 
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
 
 /**
@@ -9,38 +8,28 @@ import android.view.View;
  */
 public class ThreeDCarousel implements ViewPager.PageTransformer {
 
-    private static final float MIN_SCALE = 0.85f;
-    private static final float MIN_ALPHA = 0.5f;
+    private static final float FINAL_POSITION = 1.75f;
+    private static final float INITIAL_POSITION = 0.5f;
+    private static final float MEDIUM_POSITION = 0.625F;
+    private static final float SCALE_MINIMUN_FACTOR = 0.625F;
+    private static final float INCLINE_FACTOR = 2;
+    private static final float MOVE_FACTOR_LEFT = 0.70f;
+    private static final float MOVE_FACTOR_RIGHT = 1.3125f;
 
     @Override
     public void transformPage(View view, float position) {
-        int pageWidth = view.getWidth();
-        int pageHeight = view.getHeight();
 
-        if (position < -0.5) { // [-Infinity,-1)
-            // This page is way off-screen to the left.
-            view.setScaleX(0.45f);
-            view.setScaleY(0.45f);
-
-        } else if (position <= 1.75) { // [-1,1]
-            // Modify the default slide transition to shrink the page as well
-            if(position <= 0.625){
-                view.setScaleX(position/2 + 0.70f);
-                view.setScaleY(position/2 + 0.70f);
+        if (-INITIAL_POSITION <= position && position <= FINAL_POSITION) {
+            if(position <= MEDIUM_POSITION){
+                view.setScaleX(position/INCLINE_FACTOR + MOVE_FACTOR_LEFT);
+                view.setScaleY(position/INCLINE_FACTOR + MOVE_FACTOR_LEFT);
             } else {
-                view.setScaleX(-position/2 +1.3125f);
-                view.setScaleY(-position/2 +1.3125f);
+                view.setScaleX(-position/INCLINE_FACTOR +MOVE_FACTOR_RIGHT);
+                view.setScaleY(-position/INCLINE_FACTOR +MOVE_FACTOR_RIGHT);
             }
-            Log.d("Laneveradepablo", "posicion: " + position);
-            // Fade the page relative to its size.
-            /*view.setAlpha(MIN_ALPHA +
-                    (scaleFactor - MIN_SCALE) /
-                            (1 - MIN_SCALE) * (1 - MIN_ALPHA));*/
-
-        } else { // (1,+Infinity]
-            // This page is way off-screen to the right.
-            view.setScaleX(0.45f);
-            view.setScaleY(0.45f);
+        } else {
+            view.setScaleX(SCALE_MINIMUN_FACTOR);
+            view.setScaleY(SCALE_MINIMUN_FACTOR);
         }
     }
 }
